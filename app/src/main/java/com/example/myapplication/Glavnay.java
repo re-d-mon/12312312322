c30dfb25a8276c958d61729d3a933d06afe3ab35
re-d-mon <kironplay6@gmail.com>
2021-11-24 21:59:55 +0800

    3

--- app/src/main/java/com/example/myapplication/Glavnay.java
+++ app/src/main/java/com/example/myapplication/Glavnay.java
@@ -2,7 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Glavnay extends AppCompatActivity {
 
@@ -10,5 +18,47 @@ public class Glavnay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glavnay);
+        TextView editText_time = findViewById(R.id.textView5);
+        TextView editText_Date= findViewById(R.id.textView6);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    // получаем текущее время
+                    Date currentDate = new Date();
+// Форматирование времени как "день.месяц.год"
+                    DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+                    String dateText = dateFormat.format(currentDate);
+// Форматирование времени как "часы:минуты:секунды"
+                    DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+                    String timeText = timeFormat.format(currentDate);
+                    editText_time.post(new Runnable() {
+                        public void run() {
+                            editText_time.setText(timeText);
+                        }
+                    });
+                    editText_Date.post(new Runnable() {
+                        public void run() {
+                            editText_Date.setText(dateText);
+                        }
+                    });
+                    // отображаем в текстовом поле
+                }
+
+            }
+        };
+        // Определяем объект Thread - новый поток
+        Thread thread = new Thread(runnable);
+        // Запускаем поток
+        thread.start();
     }
+
+
+
+
+
 }
+
+
+
--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -2,7 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteTableLockedException;
+import android.graphics.Paint;
+import android.icu.text.MessagePattern;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +19,78 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn_login=findViewById(R.id.button2);
+        EditText editText_login = findViewById(R.id.editTextTextPersonName);
+        EditText editText_password = findViewById(R.id.editTextTextPersonName2);
+
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String string_login=editText_login.getText().toString();
+                String string_password = editText_password.getText().toString();
+
+                if (string_login.equals("admin")&&(string_password.equals("admin")))
+                {
+                    Intent intent_Glavnay = new Intent();
+                    intent_Glavnay.setClass(Login.this,Glavnay.class);
+
+                    startActivity(intent_Glavnay);
+                    finish();
+                }
+                else
+                {
+                    Toast toast=Toast.makeText(getApplicationContext(),"Не вырный логин или пароль",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+        editText_password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b)
+                {
+                    String string_login=editText_password.getText().toString();
+                    if (string_login.equals("Пароль"))
+                    {
+                        editText_password.setText("");
+                        editText_password.setInputType( InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD );
+                    }
+                }
+                else
+                {
+                    String string_login=editText_password.getText().toString();
+                    if (string_login.equals(""))
+                    {
+                        editText_password.setText("Пароль");
+                        editText_password.setInputType( InputType.TYPE_NUMBER_VARIATION_PASSWORD );
+                    }
+                }
+            }
+        });
+        editText_login.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b)
+                {
+                    String string_login=editText_login.getText().toString();
+                    if (string_login.equals("Email"))
+                    {
+                        editText_login.setText("");
+                    }
+                }
+                else
+                {
+                    String string_login=editText_login.getText().toString();
+                    if (string_login.equals(""))
+                    {
+                        editText_login.setText("Email");
+                    }
+                }
+            }
+        });
     }
+
 }
